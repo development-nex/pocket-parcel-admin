2bd4081edd2db3e159da4f76a52b909b6fd871da
development-nex <development@nexenstial.com>
2025-11-03 12:14:21 +0530

    refactor: add margin bottom to the breadcrumb

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -14,7 +14,9 @@ import BreadcrumbComponent from "../ui/BreadcrumbComponent";
 const PageLayout = ({ className, children, items = [] }) => {
   return (
     <div className={cn("m-5", className)}>
-      {items.length > 0 && <BreadcrumbComponent items={items} />}
+      {items.length > 0 && (
+        <BreadcrumbComponent className="mb-1" items={items} />
+      )}
       <div className="flex flex-col gap-4">{children}</div>
     </div>
   );
